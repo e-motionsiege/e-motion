0ccf98339fd9dc9bded47a4331d02d0a3146a6b1
Dalleau Théo <theoferah@gmail.com>
2019-06-28 14:06:53 +0200

    feature(registration): add registration

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -4,7 +4,6 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
@@ -13,72 +12,81 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TelType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Length;
 
-class AdminUserType extends AbstractType
+class RegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
             ->add('username', null, [
-                    'required' => true,
-                    'label' => 'Pseudo'
-                ]
-            )
-            ->add('roles', ChoiceType::class, [
-                'choices' => [
-                    'Utilisateur' => 'ROLE_USER',
-                    'Propriétaire' => 'ROLE_OWNER',
-                    'Administrateur'=>'ROLE_ADMIN'
-                ],
-                'multiple'=>true,
                 'required' => true,
-                'label' => 'Role',
-                'placeholder' => 'Choisir',
-                'expanded' => true
+                'label' => 'Pseudo*'
             ])
             ->add('email', EmailType::class, [
                 'required' => true,
-                'label' => 'Email'
+                'label' => 'Email*'
             ])
-            ->add('password', RepeatedType::class, array(
-                'type' => PasswordType:: class,
-                'first_options' => array('label' => 'Password'),
-                'second_options' => array('label' => 'Confirmé Password'),
-            ))->add('firstname', null, [
+            ->add('firstname', null, [
                 'required' => true,
-                'label' => 'Prénom'
+                'label' => 'Prénom*'
             ])
             ->add('lastname', null, [
                 'required' => true,
-                'label' => 'Nom'
+                'label' => 'Nom*'
             ])
+            ->add('password', RepeatedType::class, array(
+                'type' => PasswordType:: class,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez saisir un mot de passe',
+                    ]),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Votre mot de passe doit au moins contenir  {{ limit }} caractères',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 4096,
+                    ]),
+                ],
+                'first_options' => array('label' => 'Mot de passe*'),
+                'second_options' => array('label' => 'Confirmation du mot de passe*'),)
+            )
             ->add('birthdate', DateType::class, [
                 'required' => false,
-                'widget' => 'single_text',
-                'label' => 'Date de naissance'
+                'label' => 'Date de naissance',
+/*                'widget' => 'single_text',
+                'html5' => false,
+                'attr' => ['class' => 'js-datepicker']*/
             ])
             ->add('address', null, [
                 'required' => true,
-                'label' => 'Adresse'
+                'label' => 'Adresse*'
             ])
             ->add('city', null, [
                 'required' => true,
-                'label' => 'Ville'
+                'label' => 'Ville*'
             ])
             ->add('zipcode', null, [
                 'required' => true,
-                'label' => 'Code Postal'
+                'label' => 'Code Postal*'
             ])
             ->add('phone_number', TelType::class, [
                 'required' => true,
-                'label' => 'Téléphone'
+                'label' => 'Numéro de téléphone*'
             ])
             ->add('license_number', null, [
                 'required' => true,
-                'label' => 'Numéro de permis de conduire'
+                'label' => 'Numéro de license du permis de conduire*',
+                'constraints' => [
+                    new Length([
+                        'min' => 9,
+                        'max' => 9
+                    ])
+                ]
             ])
             ->add('submit', SubmitType::class);
-
+        ;
     }
 
     public function configureOptions(OptionsResolver $resolver)
