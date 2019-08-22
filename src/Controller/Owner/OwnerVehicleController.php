b489c4c3eb3c4731cc8bc75a55e045e4f8204f72
Dalleau Théo <theoferah@gmail.com>
2019-08-22 12:35:11 +0200

    fix some bug

--- src/Controller/Owner/OwnerVehicleController.php
+++ src/Controller/Owner/OwnerVehicleController.php
@@ -15,7 +15,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 /**
- * @Security("has_role('ROLE_OWNER') or has_role('ROLE_PROPRIETAIRE')")
+ * @Security("has_role('ROLE_OWNER') or has_role('ROLE_PROPRIETAIRE') or has_role('ROLE_USER')")
  */
 class OwnerVehicleController extends AbstractController
 {
